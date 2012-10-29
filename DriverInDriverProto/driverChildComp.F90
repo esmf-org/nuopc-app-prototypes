b71580c210edf6488bbf591809576bfc44a163b8
Gerhard Theurich <theurich@sourcespring.net>
2012-10-29 18:41:28 +0000

    - initial revision of the Driver-in-Driver prototype

---
+++ DriverInDriverProto/driverChildComp.F90
@@ -0,0 +1,74 @@
+module driverChildComp
+
+  !-----------------------------------------------------------------------------
+  ! Code that specializes generic NUOPC_Driver
+  !-----------------------------------------------------------------------------
+
+  use ESMF
+  use NUOPC
+  use NUOPC_Driver, only: &
+    driver_routine_SS             => routine_SetServices, &
+    driver_type_IS                => type_InternalState, &
+    driver_label_IS               => label_InternalState, &
+    driver_label_SetModelCount    => label_SetModelCount, &
+    driver_label_SetModelPetLists => label_SetModelPetLists, &    
+    driver_label_SetModelServices => label_SetModelServices
+  
+  implicit none
+  
+  private
+  
+  public SetServices
+  
+  !-----------------------------------------------------------------------------
+  contains
+  !-----------------------------------------------------------------------------
+
+  subroutine SetServices(gcomp, rc)
+    type(ESMF_GridComp)  :: gcomp
+    integer, intent(out) :: rc
+    
+    rc = ESMF_SUCCESS
+    
+    ! NUOPC_Driver registers the generic methods
+    call driver_routine_SS(gcomp, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+      
+    ! attach specializing method(s)
+    call ESMF_MethodAdd(gcomp, label=driver_label_SetModelCount, &
+      userRoutine=SetModelCount, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+  end subroutine
+
+  !-----------------------------------------------------------------------------
+
+  subroutine SetModelCount(gcomp, rc)
+    type(ESMF_GridComp)  :: gcomp
+    integer, intent(out) :: rc
+    
+    ! local variables
+    type(driver_type_IS)          :: is
+
+    rc = ESMF_SUCCESS
+    
+    ! query Component for its internal State
+    nullify(is%wrap)
+    call ESMF_UserCompGetInternalState(gcomp, driver_label_IS, is, rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+     
+    ! set the modelCount for driving nothing
+    is%wrap%modelCount = 0
+    
+  end subroutine
+  
+end module
