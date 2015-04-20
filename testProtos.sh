66df9cccbb288b4bcc7f6715eea188c810925ad3
Gerhard Theurich <theurich@sourcespring.net>
2015-04-20 21:04:31 +0000

    Add a new v7 prototype directory that demponstrates build- and run-time selection of components in the driver.

--- testProtos.sh
+++ testProtos.sh
@@ -28,6 +28,54 @@ echo ---------------------------------------------------------------------------
 echo STARTING: $1
 cd $1
 gmake distclean
+gmake ATM=A OCN=A,B
+echo "OCN_SELECT: A" > esm.config
+((count++))
+testList[count]=$1
+mpirun -np 4 ./$2
+if [ $? -eq 0 ]
+then
+testResult[count]="PASS"
+else
+testResult[count]="FAIL"
+fi
+echo
+gmake clean
+gmake ATM=B OCN=A,B
+echo "OCN_SELECT: A" > esm.config
+((count++))
+testList[count]=$1
+mpirun -np 4 ./$2
+if [ $? -eq 0 ]
+then
+testResult[count]="PASS"
+else
+testResult[count]="FAIL"
+fi
+echo
+gmake clean
+gmake ATM=A OCN=B
+echo "OCN_SELECT: B" > esm.config
+((count++))
+testList[count]=$1
+mpirun -np 4 ./$2
+if [ $? -eq 0 ]
+then
+testResult[count]="PASS"
+else
+testResult[count]="FAIL"
+fi
+echo FINISHED: $1
+cd ..
+echo ---------------------------------------------------------------------------
+echo
+}
+
+function TestSelectExternalProto {
+echo ---------------------------------------------------------------------------
+echo STARTING: $1
+cd $1
+gmake distclean
 ./cleanSubs.csh
 ./buildSubs.csh
 gmake ATM=A OCN=A,B,C
@@ -105,6 +153,7 @@ echo ---------------------------------------------------------------------------
 echo
 }
 
+
 TestProto v7-AsyncIOBlockingProto         asyncIOApp
 TestProto v7-AsyncIONonblockingProto      asyncIOApp
 TestProto v7-AtmOcnConProto               esmApp
@@ -119,7 +168,8 @@ TestProto v7-AtmOcnMedProto               esmApp
 TestProto v7-AtmOcnPetListProto           esmApp
 TestProto v7-AtmOcnProto                  esmApp
 TestProto v7-AtmOcnRtmTwoTimescalesProto  esmApp
-TestSelectProto v7-AtmOcnSelectExternalProto          esmApp
+TestSelectExternalProto   v7-AtmOcnSelectExternalProto    esmApp
+TestSelectProto           v7-AtmOcnSelectProto            esmApp
 TestProto v7-AtmOcnSimpleImplicitProto    esmApp
 TestProto v7-AtmOcnTransferGridProto      esmApp
 #  v7-ComponentExplorer
---
+++ v7-AtmOcnSelectProto/esm.F90
@@ -0,0 +1,215 @@
+module ESM
+
+  !-----------------------------------------------------------------------------
+  ! Code that specializes generic ESM Component code.
+  !-----------------------------------------------------------------------------
+
+  use ESMF
+  use NUOPC
+  use NUOPC_Driver, &
+    driver_routine_SS             => SetServices, &
+    driver_label_SetModelServices => label_SetModelServices
+  
+#if (defined WITH_ATM_A && !defined WITH_ATM_B)
+  use atmA, only: atmSS => SetServices
+#elif (!defined WITH_ATM_A && defined WITH_ATM_B)
+  use atmB, only: atmSS => SetServices
+#else
+#error "Exactly one valid ATM option must be specified!"
+#endif
+
+#ifdef WITH_OCN_A
+  use ocnA, only: ocnA_SS => SetServices
+#endif
+#ifdef WITH_OCN_B
+  use ocnB, only: ocnB_SS => SetServices
+#endif
+  
+  use NUOPC_Connector, only: cplSS => SetServices
+  
+  implicit none
+  
+  private
+  
+  public SetServices
+  
+  !-----------------------------------------------------------------------------
+  contains
+  !-----------------------------------------------------------------------------
+
+  subroutine SetServices(driver, rc)
+    type(ESMF_GridComp)  :: driver
+    integer, intent(out) :: rc
+    
+    rc = ESMF_SUCCESS
+    
+    ! NUOPC_Driver registers the generic methods
+    call NUOPC_CompDerive(driver, driver_routine_SS, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+      
+    ! attach specializing method(s)
+    call ESMF_MethodAdd(driver, label=driver_label_SetModelServices, &
+      userRoutine=SetModelServices, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    
+  end subroutine
+
+  !-----------------------------------------------------------------------------
+
+  subroutine SetModelServices(driver, rc)
+    type(ESMF_GridComp)  :: driver
+    integer, intent(out) :: rc
+    
+    ! local variables
+    type(ESMF_Grid)               :: grid
+    type(ESMF_Field)              :: field
+    type(ESMF_Time)               :: startTime
+    type(ESMF_Time)               :: stopTime
+    type(ESMF_TimeInterval)       :: timeStep
+    type(ESMF_Clock)              :: internalClock
+    type(ESMF_Config)             :: config
+    character(len=1)              :: ocn_select
+    character(len=80)             :: logMsg
+    type(ESMF_GridComp)           :: child
+    type(ESMF_CplComp)            :: connector
+
+    rc = ESMF_SUCCESS
+
+    ! Create and read Config
+    config = ESMF_ConfigCreate(rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call ESMF_ConfigLoadFile(config, "esm.config", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call ESMF_ConfigGetAttribute(config, value=ocn_select, label="OCN_SELECT:",&
+      rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call ESMF_ConfigDestroy(config, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    
+    ! SetServices for ATM
+    call NUOPC_DriverAddComp(driver, "ATM", atmSS, comp=child, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompAttributeSet(child, name="Verbosity", value="high", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+      
+    ! SetServices for OCN
+    if (.false.) then
+#ifdef WITH_OCN_A
+    elseif (ocn_select=="A") then
+      call NUOPC_DriverAddComp(driver, "OCN", ocnA_SS, comp=child, rc=rc)
+      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+        line=__LINE__, &
+        file=__FILE__)) &
+        return  ! bail out
+#endif
+#ifdef WITH_OCN_B
+    elseif (ocn_select=="B") then
+      call NUOPC_DriverAddComp(driver, "OCN", ocnB_SS, comp=child, rc=rc)
+      if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+        line=__LINE__, &
+        file=__FILE__)) &
+        return  ! bail out
+#endif
+    else
+      write (logMsg,*) "Not a valid ocn_select: ", ocn_select
+      call ESMF_LogSetError(ESMF_RC_ARG_BAD, msg=logMsg, &
+        line=__LINE__, &
+        file=__FILE__, &
+        rcToReturn=rc)
+      return  ! bail out
+    endif
+    call NUOPC_CompAttributeSet(child, name="Verbosity", value="high", rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+      
+    ! SetServices for atm2ocn
+    call NUOPC_DriverAddComp(driver, srcCompLabel="ATM", dstCompLabel="OCN", &
+      compSetServicesRoutine=cplSS, comp=connector, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompAttributeSet(connector, name="Verbosity", value="high", &
+      rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+      
+    ! SetServices for ocn2atm
+    call NUOPC_DriverAddComp(driver, srcCompLabel="OCN", dstCompLabel="ATM", &
+      compSetServicesRoutine=cplSS, comp=connector, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+    call NUOPC_CompAttributeSet(connector, name="Verbosity", value="high", &
+      rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+      
+    ! set the model clock
+    call ESMF_TimeIntervalSet(timeStep, m=15, rc=rc) ! 15 minute steps
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_TimeSet(startTime, yy=2010, mm=6, dd=1, h=0, m=0, &
+      calkindflag=ESMF_CALKIND_GREGORIAN, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    call ESMF_TimeSet(stopTime, yy=2010, mm=6, dd=1, h=1, m=0, &
+      calkindflag=ESMF_CALKIND_GREGORIAN, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+
+    internalClock = ESMF_ClockCreate(name="Application Clock", &
+      timeStep=timeStep, startTime=startTime, stopTime=stopTime, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+      
+    call ESMF_GridCompSet(driver, clock=internalClock, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, &
+      file=__FILE__)) &
+      return  ! bail out
+      
+  end subroutine
+
+end module
