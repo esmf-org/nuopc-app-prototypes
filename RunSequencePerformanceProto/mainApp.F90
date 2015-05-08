eec5b6bb3ab8135f181632b4fcb3fee05bd6609c
Gerhard Theurich <theurich@sourcespring.net>
2015-05-08 22:26:30 +0000

    New code for testing performance of NUOPC Layer callbacks.

---
+++ RunSequencePerformanceProto/mainApp.F90
@@ -0,0 +1,104 @@
+program mainApp
+
+  !-----------------------------------------------------------------------------
+  ! Generic ESMF Main
+  !-----------------------------------------------------------------------------
+
+  use ESMF
+
+  use driver, only: &
+    driver_SS => SetServices, &
+    stepCount
+
+  implicit none
+  
+  integer                       :: rc, userRc
+  type(ESMF_GridComp)           :: drvComp
+  real(ESMF_KIND_R8)            :: timeStart(10), timeEnd(10)
+
+  ! Initialize ESMF
+  call ESMF_Initialize(defaultCalkind=ESMF_CALKIND_GREGORIAN, rc=rc)
+  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+    
+  call ESMF_LogWrite("mainApp STARTING", ESMF_LOGMSG_INFO, rc=rc)
+  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+    
+  !-----------------------------------------------------------------------------
+  
+  ! -> CREATE THE DRIVER
+  drvComp = ESMF_GridCompCreate(name="driver", rc=rc)
+  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+    
+  ! -> SET DRIVER SERVICES
+  call ESMF_GridCompSetServices(drvComp, driver_SS, userRc=userRc, rc=rc)
+  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  if (ESMF_LogFoundError(rcToCheck=userRc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  ! INITIALIZE THE DRIVER
+  call ESMF_GridCompInitialize(drvComp, userRc=userRc, rc=rc)
+  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  if (ESMF_LogFoundError(rcToCheck=userRc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  
+  
+  call ESMF_VMWtime(timeStart(1))
+  
+  ! RUN THE DRIVER
+  call ESMF_GridCompRun(drvComp, userRc=userRc, rc=rc)
+  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  if (ESMF_LogFoundError(rcToCheck=userRc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  
+  call ESMF_VMWtime(timeEnd(1))
+  
+  print *, "timer1: / per step:", timeEnd(1)-timeStart(1), &
+    (timeEnd(1)-timeStart(1))/real(stepCount)
+
+  ! FINALIZE THE DRIVER
+  call ESMF_GridCompFinalize(drvComp, userRc=userRc, rc=rc)
+  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+  if (ESMF_LogFoundError(rcToCheck=userRc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  !-----------------------------------------------------------------------------
+  
+  call ESMF_LogWrite("mainApp FINISHED", ESMF_LOGMSG_INFO, rc=rc)
+  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+    line=__LINE__, &
+    file=__FILE__)) &
+    call ESMF_Finalize(endflag=ESMF_END_ABORT)
+
+  ! Finalize ESMF
+  call ESMF_Finalize()
+  
+end program  
