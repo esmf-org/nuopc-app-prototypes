661596a3ffcee75174957e4f6c0f0db5ed224817
Gerhard Theurich <theurich@sourcespring.net>
2018-02-28 02:48:17 +0000

    Add copyright statement.

--- AtmOcnSimpleImplicitProto/atm.F90
+++ AtmOcnSimpleImplicitProto/atm.F90
@@ -1,3 +1,13 @@
+!==============================================================================
+! Earth System Modeling Framework
+! Copyright 2002-2018, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!==============================================================================
+
 ! The ATM Component can either use default IPDv00, if the following macro is
 ! in the _off state, or use IPDv02 if in the _on state:
 #define WITHCOMPLEXDATADEPENDENCY_on
--- AtmOcnTransferGridProto/ocn.F90
+++ AtmOcnTransferGridProto/ocn.F90
@@ -1,3 +1,13 @@
+!==============================================================================
+! Earth System Modeling Framework
+! Copyright 2002-2018, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!==============================================================================
+
 ! Make sure to define test macros consistently across all source files:
 #define TEST_GRID_EDGE_WIDTHS
 #define TEST_GRID_EDGE_WIDTHS_KEEP_FACTORIZED_COORDS___off
