2bc2a1d77a69dc5b0661d2161a1b43216617585e
Gerhard Theurich <theurich@sourcespring.net>
2018-02-28 02:48:17 +0000

    Add copyright statement.

--- ComponentExplorer/nuopcExplorerDriver.F90
+++ ComponentExplorer/nuopcExplorerDriver.F90
@@ -1,3 +1,13 @@
+!==============================================================================
+! Earth System Modeling Framework
+! Copyright 2002-2018, University Corporation for Atmospheric Research, 
+! Massachusetts Institute of Technology, Geophysical Fluid Dynamics 
+! Laboratory, University of Michigan, National Centers for Environmental 
+! Prediction, Los Alamos National Laboratory, Argonne National Laboratory, 
+! NASA Goddard Space Flight Center.
+! Licensed under the University of Illinois-NCSA License.
+!==============================================================================
+
 !- conditional module blocks that support modules maybe being supplied in C
 #ifdef FRONT_H_COMP
 module compFront
