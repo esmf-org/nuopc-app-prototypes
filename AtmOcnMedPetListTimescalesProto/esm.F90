499d5fa162f3bfeb05bd8c6b8cf1aff5a743b1c7
Gerhard Theurich <theurich@sourcespring.net>
2018-04-30 19:12:47 +0000

    Log a little more info about timeStep that is being set for slow and fast time scales. Also clarify some comments.

--- AtmOcnMedPetListTimescalesProto/esm.F90
+++ AtmOcnMedPetListTimescalesProto/esm.F90
@@ -73,6 +73,7 @@ module ESM
     integer, intent(out) :: rc
     
     ! local variables
+    character(ESMF_MAXSTR)        :: name
     type(ESMF_Time)               :: startTime
     type(ESMF_Time)               :: stopTime
     type(ESMF_TimeInterval)       :: timeStep
@@ -80,15 +81,15 @@ module ESM
     integer                       :: petCount, i
     integer, allocatable          :: petList(:)
     type(ESMF_GridComp)           :: child
+    integer                       :: minutes
+    character(len=160)            :: msgString
 
     rc = ESMF_SUCCESS
     
-    ! get the petCount
-    call ESMF_GridCompGet(driver, petCount=petCount, rc=rc)
+    ! query the driver for its name and petCount
+    call ESMF_GridCompGet(driver, name=name, petCount=petCount, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
 
     ! SetServices for ATM
     allocate(petList(petCount/2-1))
@@ -98,15 +99,11 @@ module ESM
     call NUOPC_DriverAddComp(driver, "ATM", atmSS, petList=petList, &
       comp=child, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
     deallocate(petList)
     call NUOPC_CompAttributeSet(child, name="Verbosity", value="high", rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
       
     ! SetServices for OCN
     allocate(petList(petCount/2-1))
@@ -116,15 +113,11 @@ module ESM
     call NUOPC_DriverAddComp(driver, "OCN", ocnSS, petList=petList, &
       comp=child, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
     deallocate(petList)
     call NUOPC_CompAttributeSet(child, name="Verbosity", value="high", rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
 
     ! SetServices for MED
     ! the remaining two PETs that neither ATM nor OCN used
@@ -134,48 +127,40 @@ module ESM
     call NUOPC_DriverAddComp(driver, "MED", medSS, petList=petList, &
       comp=child, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
     deallocate(petList)
     call NUOPC_CompAttributeSet(child, name="Verbosity", value="high", rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
 
     ! set the driver clock
     call ESMF_TimeIntervalSet(timeStep, m=60, rc=rc) ! 60 min steps OCN cpl
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
-
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
     call ESMF_TimeSet(startTime, yy=2010, mm=6, dd=1, h=0, m=0, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
-
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
     call ESMF_TimeSet(stopTime, yy=2010, mm=6, dd=1, h=4, m=0, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
-
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
     internalClock = ESMF_ClockCreate(name="Application Clock", &
       timeStep=timeStep, startTime=startTime, stopTime=stopTime, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
-      
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
     call ESMF_GridCompSet(driver, clock=internalClock, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
-      line=__LINE__, &
-      file=__FILE__)) &
-      return  ! bail out
-      
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
+    
+    ! log the timeStep interval
+    call ESMF_TimeIntervalGet(timeStep, m=minutes, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
+    write(msgString, *) trim(name)//": set Driver slow timeStep to ", &
+      minutes, " minutes."
+    call ESMF_LogWrite(msgString, ESMF_LOGMSG_INFO, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
+    
   end subroutine
 
   !-----------------------------------------------------------------------------
@@ -185,12 +170,14 @@ module ESM
     integer, intent(out) :: rc
     
     ! local variables
-    character(ESMF_MAXSTR)              :: name
-    type(NUOPC_FreeFormat)              :: runSeqFF
-    type(ESMF_Time)                     :: startTime
-    type(ESMF_Time)                     :: stopTime
-    type(ESMF_TimeInterval)             :: timeStep
-    type(ESMF_Clock)                    :: internalClock
+    character(ESMF_MAXSTR)        :: name
+    type(NUOPC_FreeFormat)        :: runSeqFF
+    type(ESMF_Time)               :: startTime
+    type(ESMF_Time)               :: stopTime
+    type(ESMF_TimeInterval)       :: timeStep
+    type(ESMF_Clock)              :: internalClock
+    integer                       :: minutes
+    character(len=160)            :: msgString
 
     rc = ESMF_SUCCESS
     
@@ -236,7 +223,7 @@ module ESM
       file=__FILE__)) &
       return  ! bail out
     internalClock = ESMF_ClockCreate(name="MED-ATM interaction", &
-      timeStep=timeStep/2, & ! 30min steps
+      timeStep=timeStep/2, & ! fast time step half of slow time step
       startTime=startTime, stopTime=stopTime, rc=rc)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
       line=__LINE__, &
@@ -249,6 +236,16 @@ module ESM
       file=__FILE__)) &
       return  ! bail out
       
+    ! log the timeStep interval
+    call ESMF_TimeIntervalGet(timeStep/2, m=minutes, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
+    write(msgString, *) trim(name)//": set Driver fast timeStep to ", &
+      minutes, " minutes."
+    call ESMF_LogWrite(msgString, ESMF_LOGMSG_INFO, rc=rc)
+    if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
+      line=__LINE__, file=trim(name)//":"//__FILE__)) return  ! bail out
+
     ! print out info about the generated run sequence
     call NUOPC_DriverPrint(driver, orderflag=.true.)
     if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU, &
