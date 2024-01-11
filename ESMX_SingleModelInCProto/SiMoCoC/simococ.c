33d231ef5b3084bd9955737d2687be85581f1160
Gerhard Theurich <theurich@sourcespring.net>
2024-01-11 00:48:52 -0800

    Enough NUOPC C API functionality to make the case execute successfully. Very basic test. More work needed!

--- ESMX_SingleModelInCProto/SiMoCoC/simococ.c
+++ ESMX_SingleModelInCProto/SiMoCoC/simococ.c
@@ -15,70 +15,48 @@
 #include <math.h>
 
 // ESMF header -- provides access to the entire public ESMF C API
+#include "NUOPC.h"
 #include "ESMC.h"
-//#include "atmF.h"
 
 //-------------------------------------------------------------------------
 //-------------------------------------------------------------------------
 
-void Initialize(ESMC_GridComp model, ESMC_State importState,
-  ESMC_State exportState, ESMC_Clock *clock, int *rc){
-
-  // initialize return code
+void Advance(ESMC_GridComp model, int *rc){
+  // initialize return code to success
   *rc = ESMF_SUCCESS;
 }
-
 
 void Run(ESMC_GridComp model, ESMC_State importState,
   ESMC_State exportState, ESMC_Clock *clock, int *rc){
-
-  // initialize return code
+  // initialize return code to success
   *rc = ESMF_SUCCESS;
-}
 
-
-void Finalize(ESMC_GridComp model, ESMC_State importState,
-  ESMC_State exportState, ESMC_Clock *clock, int *rc){
-
-  // initialize return code
-  *rc = ESMF_SUCCESS;
+  *rc = ESMC_LogWrite("Message from inside C component RUN", ESMC_LOGMSG_INFO);
+  if (*rc!=ESMF_SUCCESS) return;  // bail out
 }
 
 //-------------------------------------------------------------------------
 //-------------------------------------------------------------------------
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-  // The SetServices and SetVM entry points must ensure to have external C
-  // linkage!
+void SetServices(ESMC_GridComp model, int *rc){
+  // initialize return code to success
+  *rc = ESMF_SUCCESS;
 
-  void SetServices(ESMC_GridComp model, int *rc){
+  *rc = NUOPC_CompDerive(model, NUOPC_ModelSetServices);
+  if (*rc!=ESMF_SUCCESS) return;  // bail out
 
-    // initialize return code
-    *rc = ESMF_SUCCESS;
+  // here call specialize once C API is available
 
-    // This is where the NUOPC_Model SetServices needs to be called once it is
-    // available for C code.
+  // for now must override RUN method, or else there is an issue because of
+  // missing advance specialization
+  *rc = ESMC_GridCompSetEntryPoint(model, ESMF_METHOD_RUN, Run, 1);
+  if (*rc!=ESMF_SUCCESS) return;  // bail out
+}
 
-    *rc = ESMC_GridCompSetEntryPoint(model, ESMF_METHOD_INITIALIZE,Initialize,1);
-    if (*rc!=ESMF_SUCCESS) return;  // bail out
-    *rc = ESMC_GridCompSetEntryPoint(model, ESMF_METHOD_RUN, Run, 1);
-    if (*rc!=ESMF_SUCCESS) return;  // bail out
-    *rc = ESMC_GridCompSetEntryPoint(model, ESMF_METHOD_FINALIZE, Finalize, 1);
-    if (*rc!=ESMF_SUCCESS) return;  // bail out
-  }
-
-  void SetVM(ESMC_GridComp model, int *rc){
-
-    // initialize return code
-    *rc = ESMF_SUCCESS;
-
-  }
-
-  #ifdef __cplusplus
-} //extern "C"
-#endif
+void SetVM(ESMC_GridComp model, int *rc){
+  // initialize return code to success
+  *rc = ESMF_SUCCESS;
+}
 
 //-------------------------------------------------------------------------
 //-------------------------------------------------------------------------
