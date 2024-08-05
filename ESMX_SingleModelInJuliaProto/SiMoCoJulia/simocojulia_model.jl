c9438029a09b42ff8f9d6c86e403ac8ef3ec2dc2
Bill Sacks <sacks@ucar.edu>
2024-08-05 17:47:06 -0600

    Add copyright header to julia code

--- ESMX_SingleModelInJuliaProto/SiMoCoJulia/simocojulia_model.jl
+++ ESMX_SingleModelInJuliaProto/SiMoCoJulia/simocojulia_model.jl
@@ -1,3 +1,13 @@
+# ==============================================================================
+# Earth System Modeling Framework
+# Copyright (c) 2002-2024, University Corporation for Atmospheric Research,
+# Massachusetts Institute of Technology, Geophysical Fluid Dynamics
+# Laboratory, University of Michigan, National Centers for Environmental
+# Prediction, Los Alamos National Laboratory, Argonne National Laboratory,
+# NASA Goddard Space Flight Center.
+# Licensed under the University of Illinois-NCSA License.
+# ==============================================================================
+
 mutable struct State
    times_called::Int
    path_to_c_library::String
